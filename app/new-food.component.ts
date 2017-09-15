ed0b9048c31d3aca085be7733e5f3279f12f7541
John <jonnygovish@gmail.com>
2017-09-15 11:27:42 +0300

    new-food.component.ts Add content for the new-food.component.ts file

--- app/new-food.component.ts
+++ app/new-food.component.ts
@@ -1,10 +1,31 @@
 import { Component} from '@angular/core';
+import{ Food } from './food.model';
 
 @Component({
 	selector: "new-food",
 	template: `
+	<div class=" container">
+		<h1>Meal Tracker</h1>
+  		<label>Enter A Meal</label>
+  		<input #newMeal>
+	</div>
+	<div>
+  		<label>Enter Detail</label>
+  		<input #newDetail>
+	</div>
+	<div>
+  		<label>Enter Calories</label>
+  		<input #newCalories>
+  		<button (click)="
+addClicked(newMeal.value, newDetail.value, newCalories);
+newMeal.value='';
+newDetail.value='';
+newMeal.value='';
+">Add</button>
+	</div>
+
 	`
 })
 export class NewFoodComponent{
-	
+
 }
