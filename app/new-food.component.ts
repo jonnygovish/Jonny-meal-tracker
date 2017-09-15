a2b539d1c13e13b25e1caddc7d12b87c92b060ff
John <jonnygovish@gmail.com>
2017-09-15 15:14:38 +0300

    new-food component
    Add content to the component

--- app/new-food.component.ts
+++ app/new-food.component.ts
@@ -1,31 +1,44 @@
-import { Component} from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import{ Food } from './food.model';
 
 @Component({
 	selector: "new-food",
 	template: `
-	<div class=" container">
-		<h1>Meal Tracker</h1>
-  		<label>Enter A Meal</label>
-  		<input #newMeal>
-	</div>
-	<div>
-  		<label>Enter Detail</label>
-  		<input #newDetail>
-	</div>
-	<div>
-  		<label>Enter Calories</label>
-  		<input #newCalories>
-  		<button (click)="
-addClicked(newMeal.value, newDetail.value, newCalories);
-newMeal.value='';
-newDetail.value='';
-newMeal.value='';
-">Add</button>
-	</div>
+	<div class="container">
+  <h1>Meal Tracker</h1>
+  <form class="form-inline">
+  <div class="jumbotron">
+      <label>Enter A Meal</label>
+      <input #newMeal>
+  </div>
+  <div>
+      <label>Enter Detail</label>
+      <input #newDetail>
+  </div>
+  <div>
+      <label>Enter Calories</label>
+      <input #newCalories>
+      <button class="btn btn-info"(click)="
+      addMeal(newMeal.value, newDetail.value, newCalories);
+      newMeal.value='';
+      newDetail.value='';
+      newMeal.value='';
+    ">Add</button>
+  </div>
+    </form>
+  
+  </div>
 
+	
 	`
 })
 export class NewFoodComponent{
+	@Output() newMealSender = new EventEmitter();
+	addMeal(name: string, details: string, calories: number){
+		var newMealToAdd: Food = new Food(name, calories, details);
+		this.newMealSender.emit(newMealToAdd);
+	}
+
+
 
 }
