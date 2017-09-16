4d514af6f3bb3e6d369edee664090773fc51fd82
John <jonnygovish@gmail.com>
2017-09-16 08:17:08 +0300

    food component link the child components Add a controller  function to push new meal in the the array

--- app/food.component.ts
+++ app/food.component.ts
@@ -4,12 +4,24 @@ import { Food} from './food.model'
 @Component({
   selector: 'food',
   template: `
-  <new-food>
+  <new-food
   (newMealSender)="addMeal($event)"
-  </new-food>
+  ></new-food>
+  <meal-list
+  [childMealList]="food"
+  ></meal-list>
   `
 })
 
 export class FoodComponent {
-  public food: Food[] =[];
+  public food: Food[] =[
+  new Food("Steak with Fries", "Lunch", 1665),
+    new Food("Grilled Cheese", "Dinner",700),
+    new Food("Yogurt and Granola", "Breakfast", 200),
+    new Food("Banana", "Snack", 100),
+    new Food("Ice Cream","Snack", 1400),
+  ];
+  addMeal(newMealFromChild: Food){
+  	this.food.push(newMealFromChild);
+  }
 }
