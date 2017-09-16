2970b7758e3e428e16f74dc161aa41d792608524
John <jonnygovish@gmail.com>
2017-09-16 08:23:35 +0300

    food module import meal-list component

--- app/food.module.ts
+++ app/food.module.ts
@@ -2,16 +2,19 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FoodComponent }   from './food.component';
 import { NewFoodComponent} from './new-food.component'
+import { FormsModule }   from '@angular/forms';
 import { Food } from './food.model';
-
+import { MealListComponent} from './meal-list.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    FormsModule,
   ],
   declarations: [ 
     FoodComponent,
     NewFoodComponent,
+    MealListComponent,
     
   ],
   bootstrap:    [ FoodComponent ]
