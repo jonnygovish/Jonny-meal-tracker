2c2835f4fd7cd1631af310d113eb5db98e841844
John <jonnygovish@gmail.com>
2017-09-17 08:44:42 +0300

    calories pipe Add a pipe element to help with filtering of meals Add content to the element

---
+++ app/calories.pipe.ts
@@ -0,0 +1,30 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Food } from './food.model';
+
+@Pipe ({
+	name: "calories"
+	pure: false
+})
+
+export class CaloriesPipe implements PipeTransform {
+	transform( input: Food[], desiredCalories){
+		var output: Food[] = [];
+		if(desiredCalories === "highCalorie" ) {
+			for(var i = 0; i < input.length; i++){
+				if(input[i].calories > 500){	
+					output.push(input[i]);
+				}
+			}
+			return output;
+		}else if(desiredCalories === "lowCalorie"){
+			for(var i = 0; i < input.length; i++){
+				if(input[i].calories < 500){
+					output.push(input[i]);
+				}
+			}
+			return output
+		}else {
+			return input;
+		}
+	}
+}
